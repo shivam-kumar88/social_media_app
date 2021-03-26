fa6fb45f6690c10b53c1cc0d2a1d04e274ad0cba
shivam <shivam882531@gmail.com>
2021-03-26 21:49:25 +0530

    secound commit just chnaged the background img of landing page

--- components/authentication/landing.js
+++ components/authentication/landing.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Text, View, Button, ImageBackground} from 'react-native'
 
-const img = { uri: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" };
+const img = { uri: "https://images.unsplash.com/photo-1591525167583-855b70ee0c4f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80" };
 
 export default function landimg({Navigation}) {
     return (
